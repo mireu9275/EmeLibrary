974567ca2921af79db1fe2bc3a62d9d4b294f8c9
HoJung Kim <69069118+mireu9275@users.noreply.github.com>
2024-05-20 16:37:31 +0900

    파일입출력 예외처리 추가

--- src/main/kotlin/kr/eme/plugin/emeLibrary/managers/FileManager.kt
+++ src/main/kotlin/kr/eme/plugin/emeLibrary/managers/FileManager.kt
@@ -3,6 +3,7 @@ package kr.eme.plugin.emeLibrary.managers
 import org.yaml.snakeyaml.DumperOptions
 import org.yaml.snakeyaml.Yaml
 import java.io.File
+import java.io.IOException
 
 class FileManager<T>(private val clazz: Class<T>, val directory: File) {
     init {
@@ -16,16 +17,25 @@ class FileManager<T>(private val clazz: Class<T>, val directory: File) {
     fun load(fileName: String): T? {
         val file = getFile(fileName)
         if (!file.exists()) return null
-        val yaml = Yaml()
-        return yaml.loadAs(file.readText(), clazz)
+        return try{
+            val yaml = Yaml()
+            yaml.loadAs(file.readText(), clazz)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
     }
     fun save(fileName: String, data: T) {
         val file = getFile(fileName)
         val option = DumperOptions().apply {
             defaultFlowStyle = DumperOptions.FlowStyle.BLOCK
         }
-        val yaml = Yaml(option)
-        file.writeText(yaml.dump(data))
+        try {
+            val yaml = Yaml(option)
+            file.writeText(yaml.dump(data))
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
     fun reload(fileName: String): T? {
         return load(fileName)
